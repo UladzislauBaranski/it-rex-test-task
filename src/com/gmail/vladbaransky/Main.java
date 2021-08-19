a04ff4ca0ed9d9c2de145d67600f017cd783f979
Vlad Baranski <vladbaransky13@gmail.com>
2021-08-19 12:16:40 +0300

    init commit

---
+++ src/com/gmail/vladbaransky/Main.java
@@ -0,0 +1,11 @@
+package com.gmail.vladbaransky;
+
+import com.gmail.vladbaransky.impl.Task2Impl;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Task2 task2 = new Task2Impl();
+        task2.f();
+    }
+}
---
+++ src/com/gmail/vladbaransky/Task2.java
@@ -0,0 +1,5 @@
+package com.gmail.vladbaransky;
+
+public interface Task2 {
+    int f();
+}
